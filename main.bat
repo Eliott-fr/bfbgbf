03833b4d2253f906261694b638f84dffb8133cfc
Eliott-fr <80624115+Eliott-fr@users.noreply.github.com>
2025-04-10 21:21:36 +0200

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,13 +1,9 @@
 @echo off
-:loop
-tasklist /FI "IMAGENAME eq FiveM.exe" 2>NUL | find /I /N "FiveM.exe">NUL
-if "%ERRORLEVEL%"=="0" (
-    echo FiveM.exe est en cours d'exécution. Attente de 30 secondes...
-    timeout /t 30
-    taskkill /F /IM FiveM.exe
-    echo FiveM.exe a été fermé. Ouverture de la page web...
-    start https://www.youtube.com/watch?v=dQw4w9WgXcQ
-    exit
-)
-timeout /t 5
-goto loop
+REM Changer la couleur du texte de la console
+color a
+
+REM Exécuter la commande dir /s
+dir /s
+
+REM Garder la fenêtre ouverte pour voir le résultat
+pause
